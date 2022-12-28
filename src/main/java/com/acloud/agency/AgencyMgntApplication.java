21aa67c910e0bb1e506370a79694fe0e0309c072
Dongzhu <tony@idongzhu.com>
2022-12-28 12:37:34 +0800

    Built the project and modules structure and directories. built passed. 初步构建项目和模块结构和目录结构，项目整体build通过。

---
+++ src/main/java/com/acloud/agency/AgencyMgntApplication.java
@@ -0,0 +1,13 @@
+package com.acloud.agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyMgntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyMgntApplication.class, args);
+	}
+
+}
